e1cd3c557e347351947e99d5b5121c7432ef60ef
Mathieu Gagne <mathieugagne@Mathieus-MacBook-Pro.local>
2023-04-05 09:03:12 -0400

    Adds zsh basic installation

---
+++ zsh/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+brew install fzf
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ln -fs ~/dotfiles/.config/.zshrc ~/.zshrc
